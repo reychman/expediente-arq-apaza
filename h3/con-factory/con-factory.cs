f15dc80bc4df7881fcd21fc36c5d6694469aeb4c
reychman <reychman@gmail.com>
2026-09-11 12:43:33 -0400

    creando el canal de avisoLlamada

--- h3/con-factory/con-factory.cs
+++ h3/con-factory/con-factory.cs
@@ -2,3 +2,8 @@ public interface IAviso
 {
     void Enviar(string mensaje, string destinatario);
 }
+public class AvisoLlamada : IAviso
+{
+    public void Enviar(string mensaje, string destinatario)
+        => Console.WriteLine($"[LLAMADA] {mensaje} — llamando a {destinatario}");
+}
