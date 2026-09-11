e63374e146500b8569db1d5e87188af16c93c899
reychman <reychman@gmail.com>
2026-09-11 14:06:11 -0400

    implementacion concretas de enlaces

--- h3/con-factory/con-factory.cs
+++ h3/con-factory/con-factory.cs
@@ -20,3 +20,48 @@ public abstract class EnlaceDePago
     public abstract string GenerarEnlace();
     public virtual bool ValidarEnlace() => !string.IsNullOrWhiteSpace(Url);
 }
+
+public class EnlaceTarjeta : EnlaceDePago, IAnulable
+{
+    public EnlaceTarjeta(decimal montoTotal) : base(montoTotal) { }
+
+    public override string GenerarEnlace()
+    {
+        Url = $"https://pagos.miempresa.bo/tarjeta/{IdEnlace}";
+        return Url;
+    }
+
+    public bool Anular()
+    {
+        Console.WriteLine($"[TARJETA] Enlace {IdEnlace} anulado.");
+        return true;
+    }
+}
+
+public class EnlaceTransferencia : EnlaceDePago, IAnulable
+{
+    public EnlaceTransferencia(decimal montoTotal) : base(montoTotal) { }
+
+    public override string GenerarEnlace()
+    {
+        Url = $"https://pagos.miempresa.bo/transferencia/{IdEnlace}";
+        return Url;
+    }
+
+    public bool Anular()
+    {
+        Console.WriteLine($"[TRANSFERENCIA] Enlace {IdEnlace} anulado.");
+        return true;
+    }
+}
+
+public class EnlaceQR : EnlaceDePago
+{
+    public EnlaceQR(decimal montoTotal) : base(montoTotal) { }
+
+    public override string GenerarEnlace()
+    {
+        Url = $"https://pagos.miempresa.bo/qr/{IdEnlace}";
+        return Url;
+    }
+}
