9f96cae85d1e94d48cefce72b60bc2fcea2b9b41
reychman <reychman@gmail.com>
2026-09-11 14:53:00 -0400

    creacion de mi clase enlaceDePago

--- h3/base/base.cs
+++ h3/base/base.cs
@@ -28,3 +28,24 @@ public class CalculoDeMora
         return montoBase * TasaDeInteresMoratorio * diasCobrables;
     }
 }
+public class EnlaceDePago
+{
+    public int IdEnlace { get; set; }
+    public string Url { get; set; } = "";
+    public decimal MontoTotal { get; set; }
+    public DateTime FechaGeneracion { get; set; }
+
+    public string GenerarEnlace(string metodoPago)
+    {
+        IdEnlace = new Random().Next(1000, 9999);
+        FechaGeneracion = DateTime.Now;
+        Url = metodoPago switch
+        {
+            "tarjeta" => $"https://pagos.miempresa.bo/tarjeta/{IdEnlace}",
+            "transferencia" => $"https://pagos.miempresa.bo/transferencia/{IdEnlace}",
+            "qr" => $"https://pagos.miempresa.bo/qr/{IdEnlace}",
+            _ => throw new ArgumentException($"Metodo de pago desconocido: {metodoPago}")
+        };
+        return Url;
+    }
+}
