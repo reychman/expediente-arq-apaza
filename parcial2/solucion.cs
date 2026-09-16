a7b1ff263546c5b38df127013048b025a4cd42fc
reychman <reychman@gmail.com>
2026-09-15 20:29:46 -0400

    corrigiendo todo el codigo sintaxis

--- parcial2/solucion.cs
+++ parcial2/solucion.cs
@@ -1,93 +1,99 @@
-//Apaza Carballo Reychman Cristopher
-//Situacion 1: aviso de vencimiento de membresia
-
+// Apaza Carballo Reychman Cristopher
+// Situacion 1: aviso de vencimiento de membresia
 using System;
+using System.Collections.Generic;
+
 namespace GimnasioFuerzaAndina
 {
     public interface ISuscriptorVencimiento
     {
         void Notificar(Socio socio);
     }
-}
-public class Socio
-{
-    public string Nombre { get; set; }
-    public string Telefono { get; set; }
-    public DateTime FechaVencimiento { get; set; }
-    public Socio(string nombre, string telefono, DateTime fechaVencimiento)
-    {
-        Nombre = nombre;
-        Telefono = telefono;
-        FechaVencimiento = fechaVencimiento;
-    }
-}
-using System.Collections.Generic;
 
-public class GestorVencimientos
-{
-    private readonly List<ISuscriptorVencimiento> _suscriptores =
-        new List<ISuscriptorVencimiento>();
-    public void Suscribir(ISuscriptorVencimiento suscriptor)
+    public class Socio
     {
-        _suscriptores.Add(suscriptor);
-    }
-    public void Desuscribir(ISuscriptorVencimiento suscriptor)
-    {
-        _suscriptores.Remove(suscriptor);
-    }
-    public void AvisarVencimiento(Socio socio)
-    {
-        foreach (var suscriptor in _suscriptores)
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+
+        public Socio(string nombre, string telefono, DateTime fechaVencimiento)
         {
-            suscriptor.Notificar(socio);
+            Nombre = nombre;
+            Telefono = telefono;
+            FechaVencimiento = fechaVencimiento;
         }
     }
-}
 
-public class NotificadorWhatsApp : ISuscriptorVencimiento
-{
-    public void Notificar(Socio socio)
+    public class GestorVencimientos
     {
-        Console.WriteLine($"[WhatsApp] Hola {socio.Nombre}, tu membresia vencio el {socio.FechaVencimiento:dd/MM/yyyy}.");
+        private readonly List<ISuscriptorVencimiento> _suscriptores =
+            new List<ISuscriptorVencimiento>();
+
+        public void Suscribir(ISuscriptorVencimiento suscriptor)
+        {
+            _suscriptores.Add(suscriptor);
+        }
+
+        public void Desuscribir(ISuscriptorVencimiento suscriptor)
+        {
+            _suscriptores.Remove(suscriptor);
+        }
+
+        public void AvisarVencimiento(Socio socio)
+        {
+            foreach (var suscriptor in _suscriptores)
+            {
+                suscriptor.Notificar(socio);
+            }
+        }
     }
-}
 
-public class RegistroVencidos : ISuscriptorVencimiento
-{
-    public void Notificar(Socio socio)
+    public class NotificadorWhatsApp : ISuscriptorVencimiento
     {
-        Console.WriteLine($"[RegistroVencidos] Se agrego a {socio.Nombre} a la lista de vencidos");
+        public void Notificar(Socio socio)
+        {
+            Console.WriteLine($"[WhatsApp] Hola {socio.Nombre}, tu membresia vencio el {socio.FechaVencimiento:dd/MM/yyyy}.");
+        }
     }
-}
 
-public class PantallaRecepcion : ISuscriptorVencimiento
-{
-    public void Notificar(Socio socio)
+    public class RegistroVencidos : ISuscriptorVencimiento
     {
-        Console.WriteLine($"[Recepcion] Aviso en pantalla: {socio.Nombre} debe renovar");
+        public void Notificar(Socio socio)
+        {
+            Console.WriteLine($"[RegistroVencidos] Se agrego a {socio.Nombre} a la lista de vencidos");
+        }
     }
-}
 
-public class ModuloPromociones : ISuscriptorVencimiento
-{
-    public void Notificar(Socio socio)
+    public class PantallaRecepcion : ISuscriptorVencimiento
     {
-        Console.WriteLine($"[Promociones] Le ofrecemos a {socio.Nombre} un 15% de descuento por renovar hoy");
+        public void Notificar(Socio socio)
+        {
+            Console.WriteLine($"[Recepcion] Aviso en pantalla: {socio.Nombre} debe renovar");
+        }
     }
-}
 
-public class Program
-{
-    public static void Main()
+    public class ModuloPromociones : ISuscriptorVencimiento
     {
-        var gestor = new GestorVencimientos();
+        public void Notificar(Socio socio)
+        {
+            Console.WriteLine($"[Promociones] Le ofrecemos a {socio.Nombre} un 15% de descuento por renovar hoy");
+        }
+    }
 
-        gestor.Suscribir(new NotificadorWhatsApp());
-        gestor.Suscribir(new RegistroVencidos());
-        gestor.Suscribir(new PantallaRecepcion());
-        gestor.Suscribir(new ModuloPromociones());
+    public class Program
+    {
+        public static void Main()
+        {
+            var gestor = new GestorVencimientos();
 
-        var socio = new Socio("Carlos Mamani","70011122", DateTime.Today);
-        gestor.AvisarVencimiento(socio);
+            gestor.Suscribir(new NotificadorWhatsApp());
+            gestor.Suscribir(new RegistroVencidos());
+            gestor.Suscribir(new PantallaRecepcion());
+            gestor.Suscribir(new ModuloPromociones());
+
+            var socio = new Socio("Carlos Mamani", "70011122", DateTime.Today);
+
+            gestor.AvisarVencimiento(socio);
+        }
     }
 }
