40cc67e9f07cb40e4089402c1deed73138a22665
reychman <reychman@gmail.com>
2026-09-22 19:47:47 -0400

    implementando los pedidos del comedor

--- integradora/refactor.cs
+++ integradora/refactor.cs
@@ -9,4 +9,8 @@ public interface INotificadorPedido
 {
     void Enviar(string mensaje);
 }
-
+public class BaseDeDatosComedor : IRepositorioPedidos
+{
+    public void GuardarPedido(string estudiante, string menu, int cantidad, decimal total)
+        => Console.WriteLine($"[BD] INSERT INTO pedidos VALUES ('{estudiante}', '{menu}', {cantidad}, {total})");
+}
